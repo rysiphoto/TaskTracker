7af7155cc78c9e3f4abf0e454f6e51aa853f7cbf
Ryan Siverson <rcsskier@mac.com>
2021-01-24 14:43:29 -0700

    button added

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,29 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import Button from './Button';
 
-const Header = () => {
+const Header = ({ title }) => {
+  const onClick = () => {
+    console.log('Click')
+  }
+
   return (
-    <header>
-      <h1>Hello from React</h1>
+    <header className='header'>
+      <h1>{title}</h1>
+      <Button
+        color='green'
+        text='Add'
+        onClick={onClick} />
     </header>
   )
+}
+
+Header.defaultProps = {
+  title: 'Task Tracker',
+}
+
+Header.propTypes = {
+  title: PropTypes.string.isRequired,
 }
 
 export default Header
