26425f7010a2447d555b39c0cb9519dcd0c5bf3f
Ryan Siverson <rcsskier@mac.com>
2021-01-24 18:35:11 -0700

    added addtask form

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import Header from './components/Header';
 import Tasks from './components/Tasks';
+import AddTask from './components/AddTask';
 import { useState } from 'react';
 
 function App() {
-
+  const [showAddTask, setShowAddTask] = useState(false)
   const [tasks, setTasks] = useState([
     {
       id: 1,
@@ -31,11 +32,38 @@ function App() {
     },
 
   ])
+
+  //Add Task
+
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() * 10000) + 1
+    const newTask = { id, ...task }
+    setTasks([...tasks, newTask])
+  }
+
+  // Delete Task
+
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id))
+  }
+
+  // Toggle Reminder
+
+  const toggleReminder = (id) => {
+    setTasks(tasks.map((task) => task.id === id ?
+      { ...task, reminder: !task.reminder } : task))
+  }
+
   return (
 
     <div className="container">
-      <Header />
-      <Tasks tasks={tasks} />
+      <Header onAdd={() => setShowAddTask(!showAddTask)} />
+      {showAddTask && <AddTask onAdd={addTask} />}
+      {tasks.length > 0 ?
+        <Tasks tasks={tasks}
+          onDelete={deleteTask}
+          onToggle={toggleReminder}
+        /> : 'No tasks to show'}
     </div>
   );
 }
--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,7 +1,11 @@
-const Tasks = ({ tasks }) => {
+import Task from './Task'
+
+const Tasks = ({ tasks, onDelete, onToggle }) => {
   return (
     <div>
-      {tasks.map((task) => (<h3 key={task.id}>{task.text}</h3>))}
+      {tasks.map((task) => (
+        <Task key={task.id} task={task} onDelete={onDelete} onToggle={onToggle} />
+      ))}
     </div>
   )
 }
